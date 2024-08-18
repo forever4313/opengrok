aec456e9c52db87cf3ab9b6e2eab51b5f40d1ac7
Kevin Dong <403125094@qq.com>
2024-08-18 13:06:40 +0800

    Update docker.sh

--- dev/docker.sh
+++ dev/docker.sh
@@ -3,7 +3,7 @@
 #
 # Build and optionally push new image to Docker hub.
 #
-# When pushing, this script uses the following secure variables:
+# When pushing, this script uses the following Travis secure variables:
 #  - DOCKER_USERNAME
 #  - DOCKER_PASSWORD
 #
@@ -11,9 +11,6 @@
 #
 
 set -e
-
-echo "Running linter"
-docker run --rm -i hadolint/hadolint:2.6.0 < Dockerfile || exit 1
 
 API_URL="https://hub.docker.com/v2"
 IMAGE="registry.cn-hangzhou.aliyuncs.com/opengrok/docker"
@@ -25,47 +22,38 @@ fi
 if [[ -n $OPENGROK_TAG ]]; then
 	VERSION="$OPENGROK_TAG"
 	VERSION_SHORT=$( echo $VERSION | cut -d. -f1,2 )
-
-	if [[ -z $VERSION ]]; then
-		echo "empty VERSION"
-		exit 1
-	fi
-
-	if [[ -z $VERSION_SHORT ]]; then
-		echo "empty VERSION_SHORT"
-		exit 1
-	fi
-
-	echo "Version: $VERSION"
-	echo "Short version: $VERSION_SHORT"
-
-	TAGS="$VERSION $VERSION_SHORT latest"
-
-	echo "Building docker image for release ($TAGS)"
-	docker buildx build \
-	    -t $IMAGE:$VERSION \
-	    -t $IMAGE:$VERSION_SHORT \
-	    -t $IMAGE:latest .
 else
-	TAGS="master"
-
-	echo "Building docker image for master"
-	docker buildx build -t $IMAGE:master .
+	VERSION="latest"
+	VERSION_SHORT="latest"
 fi
 
+if [[ -z $VERSION ]]; then
+	echo "empty VERSION"
+	exit 1
+fi
+
+if [[ -z $VERSION_SHORT ]]; then
+	echo "empty VERSION_SHORT"
+	exit 1
+fi
+
+echo "Version: $VERSION"
+echo "Short version: $VERSION_SHORT"
+
+# Build the image.
+echo "Building docker image"
+docker build \
+    -t $IMAGE:$VERSION \
+    -t $IMAGE:$VERSION_SHORT \
+    -t $IMAGE:latest .
+
 #
-# Run the image in a container. This is not strictly needed however
+# Run the image in container. This is not strictly needed however
 # serves as additional test in automatic builds.
 #
 echo "Running the image in container"
 docker run -d $IMAGE
 docker ps -a
-
-# This can only work on home repository since it needs encrypted variables.
-if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
-	echo "Not pushing Docker image for pull requests"
-	exit 0
-fi
 
 # The push only works on the main repository.
 if [[ "$OPENGROK_REPO_SLUG" != "registry.cn-hangzhou.aliyuncs.com/opengrok/docker" ]]; then
@@ -73,23 +61,29 @@ if [[ "$OPENGROK_REPO_SLUG" != "registry.cn-hangzhou.aliyuncs.com/opengrok/docke
 	exit 0
 fi
 
+# Allow Docker push for release builds only.
+if [[ -z $OPENGROK_TAG ]]; then
+	echo "OPENGROK_TAG is empty"
+	exit 0
+fi
+
 if [[ -z $DOCKER_USERNAME ]]; then
-	echo "DOCKER_USERNAME is empty, exiting"
+	echo "DOCKER_USERNAME is empty"
 	exit 1
 fi
 
 if [[ -z $DOCKER_PASSWORD ]]; then
-	echo "DOCKER_PASSWORD is empty, exiting"
+	echo "DOCKER_PASSWORD is empty"
 	exit 1
 fi
 
 # Publish the image to Docker hub.
-if [ -n "$DOCKER_PASSWORD" -a -n "$DOCKER_USERNAME" -a -n "$TAGS" ]; then
+if [ -n "$DOCKER_PASSWORD" -a -n "$DOCKER_USERNAME" -a -n "$VERSION" ]; then
 	echo "Logging into Docker Hub"
 	echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 	# All the tags need to be pushed individually:
-	for tag in $TAGS; do
+	for tag in $VERSION $VERSION_SHORT latest; do
 		echo "Pushing Docker image for tag $tag"
 		docker push $IMAGE:$tag
 	done
